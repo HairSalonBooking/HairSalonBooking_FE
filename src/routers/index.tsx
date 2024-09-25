fcd3b3ea5080676636f087a089d9f05a81a0030d
hungdz369@gmail.com <hungdz369@gmail.com>
2024-09-26 02:34:33 +0700

    deploy v2

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,9 +1,9 @@
-
 import LoginPage from '@/page/auth/Login/LoginPage'
 import RegisterPage from '@/page/auth/Register/RegisterPage'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ForgotPassWordPage from '@/page/auth/ForgotPassWord/ForgotPassWordPage'
 import Home from '@/page/customer/Home/Homepage'
+
 const AppRouter = () => {
     const token = sessionStorage.getItem("hairSalon")
     return (
@@ -13,15 +13,16 @@ const AppRouter = () => {
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
                     <Route path='/forgot_password' element={<ForgotPassWordPage />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                    <Route path='/' element={<Home />} />
+                    <Route path="*" element={<Navigate to="/login" replace />} />
                 </>
             ) : (
                 <>
+                    <Route path='/' element={<Home />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </>
             )}
-
         </Routes>
     )
 }
-export default AppRouter
+
+export default AppRouter;
