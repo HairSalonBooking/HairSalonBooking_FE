345ac88009357215425599596e44fc9b7f92bc85
Phan Văn Hùng <146726916+hungitlor123@users.noreply.github.com>
2024-09-26 02:54:12 +0700

    Update index.tsx

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,8 +1,8 @@
-import LoginPage from '@/page/auth/Login/LoginPage'
-import RegisterPage from '@/page/auth/Register/RegisterPage'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import ForgotPassWordPage from '@/page/auth/ForgotPassWord/ForgotPassWordPage'
-import Home from '@/page/customer/home/Homepage'
+import ForgotPassWordPage from "@/page/Auth/ForgotPassWord/ForgotPassWordPage"
+import LoginPage from "@/page/Auth/Login/LoginPage"
+import RegisterPage from "@/page/Auth/Register/RegisterPage"
+import Home from "@/page/customer/home/Homepage"
+import { Navigate, Route, Routes } from "react-router-dom"
 
 const AppRouter = () => {
     const token = sessionStorage.getItem("hairSalon")
@@ -13,16 +13,15 @@ const AppRouter = () => {
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
                     <Route path='/forgot_password' element={<ForgotPassWordPage />} />
-                    <Route path="*" element={<Navigate to="/login" replace />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                    <Route path='/' element={<Home />} />
                 </>
             ) : (
                 <>
-                    <Route path='/' element={<Home />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
                 </>
             )}
+
         </Routes>
     )
 }
-
-export default AppRouter;
+export default AppRouter
