8862d7ba011e49410bc51c1125b70a1c07a2a160
Phan Văn Hùng <146726916+hungitlor123@users.noreply.github.com>
2024-10-01 01:31:05 +0700

    Update index.tsx

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -3,7 +3,6 @@ import LoginPage from '@/page/auth/Login/LoginPage'
 import RegisterPage from '@/page/auth/Register/RegisterPage'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ForgotPassWordPage from '@/page/auth/ForgotPassWord/ForgotPassWordPage'
-
 import { useAppSelector } from '@/services/store/store'
 import Home from '@/page/customer/home/HomePage'
 import Profile from '@/page/customer/User/UserProfilePage'
@@ -20,8 +19,8 @@ const AppRouter = () => {
                 <>
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
+                    <Route path='/forgot_password' element={<ForgotPassWordPage />} />
                     <Route path='/profile' element={<Profile />} />
-
                     <Route path='/booking' element={<BookingPage />} />
                     <Route path='/about' element={<AboutPage />} />
                     <Route path="*" element={<Navigate to="/" replace />} />
@@ -33,9 +32,9 @@ const AppRouter = () => {
                     <Route path='/profile' element={<Profile />} />
                     {isCustomer && (
 
-
                         <>
                             <Route path='/home' element={<Home />} />
+
                         </>
                     )}
                 </>
