b24d0f9192b0c8c8abed885bddc433d81385e343
ThanhTai1309 <tai73559@gmail.com>
2024-10-06 23:41:01 +0700

    feat : stylist image v2

--- src/components/layout/Section/MakeupArtistSection.tsx
+++ src/components/layout/Section/MakeupArtistSection.tsx
@@ -35,7 +35,7 @@ const MakeupArtistSection = () => {
                         <div
                             key={index}
                             className="shadow-lg p-6 gap-8 rounded-lg bg-white hover:bg-[#e7cbaa] transition-transform duration-300">
-                            <img src={stylist.image ?? ""} alt="Danica Lewis" className="w-full h-80 object-cover rounded-lg mb-4" />
+                            <img src={stylist?.image} alt="Danica Lewis" className="w-full h-80 object-cover rounded-lg mb-4" />
                             <h3 className="text-2xl font-bold text-black">{stylist.firstName} {stylist.lastName}</h3>
                             <p className="text-lg text-gray-600">{stylist.positionId} </p>
                         </div>))}
