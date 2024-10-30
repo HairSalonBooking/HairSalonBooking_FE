e31497bc953c3606f20d75d991e90d836e67aa76
hungdz369@gmail.com <hungdz369@gmail.com>
2024-10-30 15:27:02 +0700

    fixx router

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -66,6 +66,7 @@ const AppRouter = () => {
                     <Route path='/about' element={<AboutPage />} />
                     <Route path='/services/:id' element={<ServiceDetailPage />} /> */}
                     <Route path="*" element={<Navigate to="/not-found" replace />} />
+                    <Route path='/' element={<Home />} />
 
 
                     {isCustomer && (
